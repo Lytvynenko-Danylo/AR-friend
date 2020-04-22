e76b381b1a7e945fbfede871fc37f4cc8ef12bd2
Rufield <biglazycatukr@gmail.com>
2020-04-22 22:33:53 +0300

    project

---
+++ Assets/MyScripts/FinalController.cs
@@ -0,0 +1,98 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace MyScripts
+{
+    public class FinalController : MonoBehaviour
+    {
+        [SerializeField]
+        Animator animator;
+        private static string[] commands = { "wake up", "make up", "hi", "dance", "then", "dans", "dence", "stop", "store" };
+        private static string[] danceStyles = { "Jazz Dancing", "Breakdance Uprock Var 1", "Hokey Pokey",
+        "Swing Dancing", "Chicken Dance", "Dancing Maraschino Step" };
+        private int count = 0;
+        private bool CharapterShowed;
+
+        public GameObject Explosion;
+        void Start()
+        {
+            gameObject.SetActive(false);
+            animator.GetComponent<Animator>();
+            CharapterShowed = false;
+            Explosion.SetActive(false);
+        }
+
+        public void Actions(string usercommands)
+        {
+            string text = usercommands.Trim().ToLower();
+            foreach (var item in commands)
+            {
+                if (text.Contains(item))
+                {
+                    text = item;
+                    break;
+                }
+            }
+
+            switch (text)
+            {
+                case "wake up":
+                case "make up":
+                    if (!CharapterShowed)
+                    {
+                        //chara.
+                        //animator.SetBool("ShowFinal", true);
+                        Explosion.SetActive(true);
+                        Invoke("ShowFinal", 2f);
+                    }
+                    break;
+                case "hi":
+                    animator.Play("hi", -1, 0f);
+                    break;
+                case "dance":
+                case "dans":
+                case "dence":
+                case "then":
+                    animator.Play(danceStyles[count], -1, 0f);
+                    count++;
+                    if (count >= danceStyles.Length)
+                    {
+                        count = 0;
+                    }
+                    // for (int i = 0; i < danceStyles.Length; i++)
+                    // {
+                    //     count = i;
+                    //     if (count == 0)
+                    //     {
+                    //         animator.Play(danceStyles[count], -1, 0f);
+                    //     }
+                    //     else
+                    //     {
+                    //         Invoke("ShowDance", 15f);
+                    //     }
+                    // }
+                    break;
+                case "stop":
+                case "store":
+                    animator.Play("Idle", -1, 0f);
+                    break;
+                default:
+                    break;
+            }
+        }
+        private void ShowFinal()
+        {
+            Explosion.SetActive(false);
+            gameObject.SetActive(true);
+            CharapterShowed = true;
+        }
+        // private void ShowDance()
+        // {
+        //     animator.Play(danceStyles[count], -1, 0f);
+        //     if (count >= danceStyles.Length)
+        //     {
+        //         count = 0;
+        //     }
+        // }
+    }
+}
---
+++ Assets/MyScripts/TouchController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class TouchController : MonoBehaviour
+{
+    public GameObject Final;
+    public GameObject Explosion;
+    public Animator animator;
+    private GameObject DanceBtn;
+    private GameObject LampBtn;
+    private GameObject DanceText;
+    private int count = 0;
+    private static string[] danceStyles = { "Jazz Dancing", "Breakdance Uprock Var 1", "Hokey Pokey",
+        "Swing Dancing", "Chicken Dance", "Dancing Maraschino Step" };
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+#if UNITY_EDITOR
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.name.ToString() == "MagicLamp" && Final.activeInHierarchy == false)
+                {
+                    Explosion.SetActive(true);
+                    Invoke("ShowFinal", 2f);
+                }
+                else if (hit.transform.name.ToString() == "Final" && Final.activeInHierarchy == true)
+                {
+                    animator.Play("hi", -1, 0f);
+                }
+            }
+        }
+#elif UNITY_ANDROID
+        if ((Input.GetTouch(0).phase == TouchPhase.Stationary) ||
+        (Input.GetTouch(0).phase == TouchPhase.Moved) && (Input.GetTouch(0).deltaPosition.magnitude < 1.2f))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                // if (hit.transform.name.ToString() == "MagicLamp" && Final.activeInHierarchy == false)
+                // {
+                //     Explosion.SetActive(true);
+                //     Invoke("ShowFinal", 2f);
+                // }
+                 if (hit.transform.name.ToString() == "Final" && Final.activeInHierarchy == true)
+                {
+                    animator.Play("hi", -1, 0f);
+                }
+            }
+        }
+        if((Input.GetTouch(0).phase == TouchPhase.Moved) && (Input.GetTouch(0).deltaPosition.magnitude > 3f)) 
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.name.ToString() == "MagicLamp" && Final.activeInHierarchy == false)
+                {
+                    Explosion.SetActive(true);
+                    Invoke("ShowFinal", 2f);
+                }
+                 if (hit.transform.name.ToString() == "Final" && Final.activeInHierarchy == true)
+                {
+                    animator.Play(danceStyles[count], -1, 0f);
+                                             count++;
+                    if (count >= danceStyles.Length)
+                    {
+                        count = 0;
+                    }
+                }
+            }
+        }
+#endif
+    }
+
+    private void ShowFinal()
+    {
+        Explosion.SetActive(false);
+        Final.SetActive(true);
+    }
+}
